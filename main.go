2a70623daeb020703049997656d2bcfdbf0919ca
Andrey Ryzhuk <senseandrey@gmail.com>
2021-01-01 05:33:38 +0200

    dep fix

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
